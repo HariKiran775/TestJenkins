9e00f57d993d3ab31969add8f9035e0209693763
HariKiran775 <84718447+HariKiran775@users.noreply.github.com>
2023-01-25 11:51:29 +0530

    Update Testcode.java

--- Testcode.java
+++ Testcode.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Testcode {
     public static void main(String []args)
     {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
     }
 }
